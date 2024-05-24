c2f5ce604ea61ccb2014a71afbc71a61ecd08514
warnishpl <162641978+warnishpl@users.noreply.github.com>
2024-05-25 00:36:11 +0200

    babel, stylelint, stylelint-order added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import styled from 'styled-components';
 import { AddContactButton } from './components/AddContactButton/AddContactButton.jsx';
 import { ContactsHeader } from './components/ContactsHeader/ContactsHeader.jsx';
 import { SearchInput } from './components/SearchInput/SearchInput.jsx';
@@ -8,13 +8,27 @@ import { ContactItemsMap } from './components/ContactItemsMap/ContactItemsMap.js
 
 function App() {
 	return (
-		<main>
+		<Main>
 			<SearchInput />
 			<AddContactButton />
 			<ContactsHeader />
 			<ContactItemsMap />
-		</main>
+		</Main>
 	);
 }
+
+const Main = styled.main`
+	display: grid;
+	grid-template-columns: [margin-left] 30px [content-start] 1fr [content-mid] auto [content-end] 30px [margin-right];
+	grid-template-rows: [margin-top] 30px [content-top] 70px [header-top] auto [header-bottom] auto [content-mid] 1fr [content-bottom] 30px [margin-bottom];
+	min-width: 350px;
+
+	@media (min-width: 992px) {
+		display: grid;
+		grid-template-columns: [margin-left] 30px [content-start] auto [content-mid] 60dvw [content-end] 30px [margin-right];
+		grid-template-rows: [margin-top] 30px [content-top] 70px [header-top] auto [header-bottom] auto [content-mid] 1fr [content-bottom] 30px [margin-bottom];
+	}
+`;
+
 
 export default App;
