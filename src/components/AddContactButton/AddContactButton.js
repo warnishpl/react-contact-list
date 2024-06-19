e755adc437fbd0cf1d7556bdf53647b4cd2faf97
Kacper <kacper.holik@gmail.com>
2024-06-19 11:09:08 +0200

    AddContactButtonContextValue doesnt work (#1)

--- src/components/AddContactButton/AddContactButton.js
+++ src/components/AddContactButton/AddContactButton.js
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import { ReactComponent as AddIcon } from '../../assets/add-icon.svg';
 import { Wrapper, Button, P } from './AddContactButton.styles.js';
-// import { AddContactForm } from './../AddContactForm/AddContactForm.js';
+import { AddContactButtonContext } from '../../context/isAddContactButtonClicked.js';
 
-export function AddContactButton({ onClick }) {
+export function AddContactButton() {
+	const AddContactButtonContextValue = useContext(AddContactButtonContext);
 	return (
 		<Wrapper>
-			<Button onClick={onClick}>
+			<Button
+				onClick={AddContactButtonContextValue.handleAddContactButtonClick}
+			>
 				<AddIcon />
 				<P>Dodaj kontakt</P>
 			</Button>
---
+++ src/context/isAddContactButtonClicked.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from 'react';
+
+export const AddContactButtonContext = createContext({
+	isAddContactButtonClicked: false,
+	setIsAddContactButtonClicked: () => {},
+	handleAddContactButtonClick: () => {},
+});
+
+export const AddContactButtonProvider = ({ children }) => {
+	const [isAddContactButtonClicked, setIsAddContactButtonClicked] =
+		useState(false);
+	const handleAddContactButtonClick = () => {
+		setIsAddContactButtonClicked(!isAddContactButtonClicked);
+	};
+	return (
+		<AddContactButtonContext.Provider
+			value={
+				(isAddContactButtonClicked,
+				setIsAddContactButtonClicked,
+				handleAddContactButtonClick)
+			}
+		>
+			{children}
+		</AddContactButtonContext.Provider>
+	);
+};
